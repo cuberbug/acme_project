f09a0a5f5adfd4e778b8c55d0fb1e7db80e668d0
Александр Кубышин <gigobug@gmail.com>
2023-08-17 02:47:29 +0300

    Start project

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    path('', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
-    path('birthday/', include('birthday.urls')),
+    path('birthday/', include('birthday.urls', namespace='birthday')),
 ]
