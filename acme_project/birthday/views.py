e0aa7b8c32b53a7f22e71e1c1aa0230a0ba298a5
Александр Кубышин <gigobug@gmail.com>
2023-08-22 02:25:55 +0300

    5

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,33 +1,11 @@
-# from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
-# from django.core.paginator import Paginator
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import BirthdayForm
 from .models import Birthday
-# from .utils import calculate_birthday_countdown
-
-
-class BirthdayMixin:
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
-
-
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-
-
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
@@ -36,41 +14,27 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-# def birthday(request, pk=None):
-#     if pk is not None:
-#         instance = get_object_or_404(Birthday, pk=pk)
-#     else:
-#         instance = None
-#     form = BirthdayForm(
-#         request.POST or None,
-#         files=request.FILES or None,
-#         instance=instance,
-#     )
-#     context = {'form': form}
-#     if form.is_valid():
-#         form.save(
-#         birthday_countdown = calculate_birthday_countdown(
-#             form.cleaned_data['birthday']
-#         )
-#         context.update({'birthday_countdown': birthday_countdown})
-#     return render(request, 'birthday/birthday.html', context)
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
 
 
-# def birthday_list(request):
-#     birthdays = Birthday.objects.order_by('id')
-#     paginator = Paginator(birthdays, 10)
-
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     context = {'page_obj': page_obj}
-#     return render(request, 'birthday/birthday_list.html', context)
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
 
 
-# def delete_birthday(request, pk):
-#     instance = get_object_or_404(Birthday, pk=pk)
-#     form = BirthdayForm(instance=instance)
-#     context = {'form': form}
-#     if request.method == 'POST':
-#         instance.delete()
-#         return redirect('birthday:list')
-#     return render(request, 'birthday/birthday.html', context)
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
